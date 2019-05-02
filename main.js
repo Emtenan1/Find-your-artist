013c0d5e552a511405cdcdf1ea099f5a6378d664
Emtenan Alnasser <emtenan95.1@hotmail.com>
2019-05-02 10:27:08 +0300

    Final Project

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-   $(".H2").hide(),
+   $(".Background").hide(),
+   $(".Background1").hide(),
     $("form").on("submit", ONclick)
 
     function ONclick() {
@@ -10,18 +11,24 @@ $(document).ready(function () {
             method: "GET",
             url: "https://theaudiodb.com/api/v1/json/195003/search.php?s=" + Name,
             success: function (result) {
-                $('.body').empty();
-                $(".body").append($("<p/>").text("Name: "+" "+result.artists[0].strArtist));
-                $(".body").append($("<p/>").text("Gender: "+result.artists[0].strGender));
-                $(".body").append($("<p/>").html("Born Year: "+" "+result.artists[0].intBornYear));
-                $(".body").append($("<p/>").html("Formed Year: "+" "+result.artists[0].intFormedYear));
-                $(".body").append($("<p/>").text("Style: "+" "+result.artists[0].strStyle));
-                $(".body").append($("<p/>").text("Mood: "+" "+result.artists[0].strMood));
-                $(".body").append($("<p/>").text("Twitter: "+" "+result.artists[0].strTwitter));
-                $(".H2").show()
+                $("form").hide(),
+                $('.Information').empty();
+                $('#header').empty();
+                
+                $("#header").append($("<h1/>").text(result.artists[0].strArtist));
+                $(".Information").append($("<p/>").text("Gender: "+result.artists[0].strGender));
+                $(".Information").append($("<p/>").html("Born Year: "+" "+result.artists[0].intBornYear));
+                $(".Information").append($("<p/>").html("Formed Year: "+" "+result.artists[0].intFormedYear));
+                $(".Information").append($("<p/>").text("Style: "+" "+result.artists[0].strStyle));
+                $(".Information").append($("<p/>").text("Mood: "+" "+result.artists[0].strMood));
+                $(".Information").append($("<p/>").text("Twitter: "+" "+result.artists[0].strTwitter));
+                $(".discription").append($("<p3/>").text(result.artists[0].strBiographyEN));
+                $(".Background").show()
+                $(".Background1").show()
                 console.log(result)
+
               if(result.artists[0].strGender==="Female"){
-                  $("#img").attr("src", "female.png")
+                  $("#img").attr("src", "female.png").css({'height':'50px', 'width':'50px'})
               }
                else{
                 $("#img").attr("src","male.png")
